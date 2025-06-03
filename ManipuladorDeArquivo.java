54c815b97612f8302e56edf10cf11958d0698f40
Rayan Junio <rayanjunior234@gmail.com>
2025-06-03 20:46:15 -0300

    add new class to read the created file

---
+++ ManipuladorDeArquivo.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+public class ManipuladorDeArquivo {
+  private final String caminhoArquivo = "palavras.txt";
+  private final int MAX_PALAVRAS = 5;
+
+  public ManipuladorDeArquivo() {}
+
+  public String lerArquivo() {
+    Random aleatorio = new Random();
+    int valor = aleatorio.nextInt(MAX_PALAVRAS);
+    String linha;
+
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo));
+      String palavra = "";
+      int i = 0;
+
+      while((linha = reader.readLine()) != null) {
+        if(i == valor) palavra = linha;
+        i++;
+      }
+      return palavra;
+
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
