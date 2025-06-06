f99c221810ab73f55fb55bb49da11d81c75af0d0
Rayan Junio <rayanjunior234@gmail.com>
2025-06-05 23:23:33 -0300

    add server-side logic for handling hangman game

--- Servidor.java
+++ Servidor.java
@@ -3,89 +3,139 @@ import java.io.*;
 /*
  *  Servidor TCP - versão simplificada para o laboratório de redes
  *
- *  (C) Prof. Dr. Ulisses Rodrigues Afonseca
  *
  *  Este é um programa servidor simples, sem thread, para atender a um
  *  protocolo genérico implementado na classe Protocolo. O código original
  *  foi tirado do SUN Java Tutorial.
  *
  */
- 
+
 public class Servidor {
-   private static int porta = 4444;
-   private static final String versao = "1.0";
-   private static final String discoPrincipal = "/";
-   public static void main(String args[]) {
-      ServerSocket serverSocket = null;
-      boolean run = true;
-      try {
-         serverSocket = new ServerSocket(4444);
-      } catch (IOException e)  {
-         System.out.println("Nao foi possivel escutar na porta " + porta + "." + e);
-         System.out.println("Erro: " + e);
-         System.exit(1);
-      }
+    private static int porta = 4444;
+    private static final int MAX_TENTATIVAS = 6;
 
-      while(run) {
-          Socket clientSocket = null;
-          try {
-             clientSocket = serverSocket.accept();
-          } catch (IOException e)  {
-             System.out.println("Falha ao aceitar conexao! Erro: " + e);
-             System.exit(1);
-          }
-          
-          System.out.println("Conexão estabelecida com "+clientSocket.getRemoteSocketAddress().toString());
-          
-          try {
-             BufferedReader inStream  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             PrintWriter    outStream = new PrintWriter(clientSocket.getOutputStream(),true);
-             String inputLine, outputLine;
+    public static void main(String args[]) {
+        ServerSocket serverSocket = null;
+        boolean run = true;
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.out.println("Nao foi possivel escutar na porta " + porta + "." + e);
+            System.out.println("Erro: " + e);
+            System.exit(1);
+        }
 
-             while ((inputLine = inStream.readLine()) != null) {
-				 
-				// aqui vamos tratar a requisição realizada pelo cliente 
-				 
-				switch (inputLine) {
-					case "getVersion":
-						outputLine = versao;
-						break;
-					case "getServerTime":
-					    java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH:mm:ss");
-						java.util.Date hora = java.util.Calendar.getInstance().getTime();
-						outputLine = sdf.format(hora);
-						break;
-					case "getFreeSpace":
-					    java.io.File principal = new java.io.File(discoPrincipal);
-						long freeespace = principal.getFreeSpace();
-						outputLine = "Espaço disponível = "+freeespace;
-						break;
-					case "end": 
-					    outputLine = "goodbye";
-						break;
-					default:
-					    outputLine = "mensagem desconhecida!";
-				}
-                //aqui vamos enviar a resposta produzida para o cliente
-                outStream.println(outputLine);
-                outStream.flush();
-                if (outputLine.equals("goodbye"))
-                   break;
-             }
-             outStream.close();
-             inStream.close();
-             clientSocket.close();
-          } catch (IOException e) {
-                System.out.println("Erro ao tratar as requisições! Erro:"+e);
+        while (run) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Falha ao aceitar conexao! Erro: " + e);
                 System.exit(1);
-          }
-       }
-       try {
-        serverSocket.close();
-       } catch (IOException e) {
-                System.out.println("Erro ao encerrar o servidor"+e);
+            }
+
+            System.out.println("Conexão estabelecida com " + clientSocket.getRemoteSocketAddress().toString());
+
+            try {
+                BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter outStream = new PrintWriter(clientSocket.getOutputStream(), true);
+                String inputLine;
+                ManipuladorDeArquivo arquivo = new ManipuladorDeArquivo();
+
+                while ((inputLine = inStream.readLine()) != null) {
+                    inputLine = inputLine.toLowerCase();
+
+                    // trata a requisição do cliente
+                    switch (inputLine) {
+                        case "iniciarjogo":
+                            iniciarJogo(arquivo, inStream, outStream);
+                            break;
+                        case "end":
+                            outStream.println("goodbye");
+                            break;
+                        default:
+                            outStream.println("Mensagem desconhecida! Digite 'iniciarjogo' ou 'end'");
+                    }
+                }
+                outStream.close();
+                inStream.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Erro ao tratar as requisições! Erro:" + e);
                 System.exit(1);
-       }
+            }
+        }
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao encerrar o servidor" + e);
+            System.exit(1);
+        }
+    }
+
+    public static void iniciarJogo(ManipuladorDeArquivo arquivo, BufferedReader inStream, PrintWriter outStream) throws IOException {
+        String palavra = arquivo.lerArquivo();
+        String palavraDescoberta = "_".repeat(palavra.length());
+        int tentativasRestantes = MAX_TENTATIVAS;
+
+        outStream.println("Iniciando jogo... A palavra tem " + palavra.length() + " letras: " + palavraDescoberta);
+
+        while (true) {
+            String tentativa = inStream.readLine();
+            if (tentativa.isBlank()) {
+                outStream.println("Tentativa inválida");
+                continue;
+            }
+
+            tentativa = tentativa.trim().toLowerCase();
+
+            if (tentativa.equals("end")) {
+                outStream.println("Encerrando jogo...");
+                break;
+            }
+
+            if (tentativa.length() != 1) {
+                outStream.println("Tentativa inválida");
+                continue;
+            }
+
+            char letra = tentativa.charAt(0);
+
+            if(palavraDescoberta.contains(String.valueOf(letra))) {
+                outStream.println("Letra '" + letra + "' já foi utilizada");
+                continue;
+            }
+
+            boolean contem = false;
+            StringBuilder novaPalavra = new StringBuilder(palavraDescoberta);
+
+            for (int i = 0; i < palavra.length(); i++) {
+                if (palavra.charAt(i) == letra) {
+                    novaPalavra.setCharAt(i, letra);
+                    contem = true;
+                }
+            }
+
+            palavraDescoberta = novaPalavra.toString();
+
+            if (contem && !palavraDescoberta.equals(palavra))
+                outStream.println("Acertou! Palavra: " + palavraDescoberta);
+            else if (!contem) {
+                tentativasRestantes--;
+
+                if(tentativasRestantes > 0) {
+                    outStream.println("Errou! Você possui " + tentativasRestantes + " tentativas.");
+                } else {
+                    outStream.println("Fim de jogo! Você não possui mais tentativas.");
+                    break;
+                }
+            }
+
+            if (palavraDescoberta.equals(palavra)) {
+                outStream.println("Fim de jogo! Você acertou! A palavra era " + palavraDescoberta);
+                break;
+            }
+        }
     }
 }
 
